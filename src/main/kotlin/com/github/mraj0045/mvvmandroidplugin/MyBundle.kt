9344da081a5c9f8618212932a7b60c8ead3f6452
GitHub Action <action@github.com>
2020-12-19 10:59:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/mraj0045/mvvmandroidplugin/MyBundle.kt
+++ src/main/kotlin/com/github/mraj0045/mvvmandroidplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mraj0045.mvvmandroidplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/mraj0045/mvvmandroidplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mraj0045.mvvmandroidplugin.services
+
+import com.github.mraj0045.mvvmandroidplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
